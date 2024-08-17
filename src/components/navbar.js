6bacf07951a70269cc215e15fa26d149d3a876af
Rahul.Chavali <rahul.chavali1@gmail.com>
2024-08-16 23:57:44 -0500

    Mobile View

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navbar.css';
 import presentationImage from './Translate.png';  // Import your presentation image
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="logo">Translate</div>
       <div className="presentation-logo-container">
         <img src={presentationImage} alt="Presentation Logo" className="presentation-logo" />
       </div>
-      <ul className="nav-links">
+      <button className="hamburger" onClick={toggleMenu}>
+        ☰
+      </button>
+      <ul className={`nav-links ${isOpen ? 'open' : ''}`}>
         <li><a href="#features">Features To Add</a></li>
         <li><a href="#build">What I Have Built</a></li>
         <li><a href="#footer">Contact</a></li>
